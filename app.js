247b546fb8c957512d1621d9609dde8ea5edef94
Stefan Koev <st.koev@outlook.com>
2022-09-21 21:06:32 +0300

    open modal setup

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ function getElement(selection) {
 }
 
 function Gallery(element) {
+  this.container = element;
   this.list = [...element.querySelectorAll(".img")];
 
   // target
@@ -18,7 +19,23 @@ function Gallery(element) {
   this.closeBtn = getElement(".close-btn");
   this.nextBtn = getElement(".next-btn");
   this.prevBtn = getElement(".prev-btn");
+  // self reference
+  // let self = this;
+
+  // bind functions
+  // this.openModal = this.openModal.bind(this);
+  // container event
+  this.container.addEventListener(
+    "click",
+    function (e) {
+      this.openModal();
+    }.bind(this)
+  );
 }
+
+Gallery.prototype.openModal = function () {
+  this.modal.classList.add("open");
+};
 
 const nature = new Gallery(getElement(".nature"));
 const city = new Gallery(getElement(".city"));
