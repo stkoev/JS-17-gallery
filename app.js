5ba1d2faf24e5f4da969dec5ea68ede2e9daa1d7
Stefan Koev <st.koev@outlook.com>
2022-09-22 15:23:28 +0300

    open modal complete

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function Gallery(element) {
   // target
   this.modal = getElement(".modal");
   this.modalImg = getElement(".main-img");
+  this.imageName = getElement(".image-name");
   this.modalImages = getElement(".modal-images");
   this.closeBtn = getElement(".close-btn");
   this.nextBtn = getElement(".next-btn");
@@ -28,13 +29,39 @@ function Gallery(element) {
   this.container.addEventListener(
     "click",
     function (e) {
-      this.openModal();
+      // this.openModal();
+      if (e.target.classList.contains("img")) {
+        console.log(e.target);
+        this.openModal(e.target, this.list);
+      }
     }.bind(this)
   );
 }
 
-Gallery.prototype.openModal = function () {
+Gallery.prototype.openModal = function (selectedImage, list) {
+  this.setMainImage(selectedImage);
+  this.modalImages.innerHTML = list
+    .map((img) => {
+      return `
+          <img
+            src="${img.src}"
+            title="${img.title}"
+            class="${
+              selectedImage.dataset.id === this.imageName.dataset.id
+                ? "modal-img selected"
+                : "modal-img"
+            }"
+            data-id="${img.dataset.id}"
+            alt="city"
+          />`;
+    })
+    .join("");
   this.modal.classList.add("open");
+};
+
+Gallery.prototype.setMainImage = function (selectedImage) {
+  this.modalImg.src = selectedImage.src;
+  this.imageName.textContent = selectedImage.title;
 };
 
 const nature = new Gallery(getElement(".nature"));
